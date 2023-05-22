0ef1bde27bda6834149d1c46d78e5b08dffc3c4b
joyce-fullstack <joyce@fullstack.ph>
2023-05-22 22:08:37 +0800

    Add console logs

--- pages/api/open-graph-image.ts
+++ pages/api/open-graph-image.ts
@@ -14,6 +14,8 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
           args: chrome.args,
           executablePath: await chrome.executablePath,
           headless: chrome.headless,
+          defaultViewport: chrome.defaultViewport,
+          ignoreHTTPSErrors: true,
         }
       : {
           args: [],
@@ -22,17 +24,24 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         };
 
     const browser = await puppeteer.launch(options);
+    console.log("browser");
     const page = await browser.newPage();
+    console.log("page");
     await page.setViewport({ width: 640, height: 360 });
+    console.log("page1");
     await page.goto(url, { waitUntil: "networkidle0" });
+    console.log("page2");
     const image = await page.screenshot({ type: "png" });
+    console.log("image");
     await browser.close();
+    console.log("image close");
 
     res.statusCode = 200;
     res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
     res.setHeader("Content-Type", `image/png`);
     res.end(image);
   } catch (error) {
+    console.log("error", error);
     res.json({
       status: "error",
       data: error || "Something went wrong",
