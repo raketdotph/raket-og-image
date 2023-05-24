7c7c9c9ce6cad8c520999d8b1e57231d40cfe956
joyce-fullstack <joyce@fullstack.ph>
2023-05-24 20:26:52 +0800

    Add puppeteer args

--- pages/api/open-graph-image.ts
+++ pages/api/open-graph-image.ts
@@ -13,8 +13,10 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         "--no-sandbox",
         "--single-process",
         "--no-zygote",
-        '--disable-extensions'
+        '--use-gl=egl',
+        '--disable-dev-shm-usage'
       ],
+      ignoreDefaultArgs: ['--disable-extensions'],
       headless: true,
       ignoreHTTPSErrors: true,
       executablePath: process.env.PUPPETEER_EXECUTABLE_PATH
